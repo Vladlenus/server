e73959b71a412ea3c8c1c285c91de9dbf2e22c9e
Vladlenus <110651671+Vladlenus@users.noreply.github.com>
2023-01-28 18:29:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ for (const key in content) {
     
     `
 }
-list.innerHTML+=`<li>${content.name}</li>`
+
 }
 {/* <iframe width="560" height="315" src="${content[key].datab}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
 ass()
